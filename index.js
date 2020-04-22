b57248716aac32259bf299cd8423d6ee7f0fd17e
jermain119 <jermain119@gmail.com>
2020-04-22 15:34:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+// const can be any datatype. const means you cant change the variable is it non changeble
+let mostProfitableNeighborhood = 'Chelsea'
+let ProfitableNeighborhood = 'Upper West Side'
+let companyCeo = 'Susan Smith'
